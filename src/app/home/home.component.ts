0e95915b49e4b43e19c4ed9c9f677cf418d4e5d6
alex-skinner <alex.skins@gmail.com>
2019-04-15 14:35:18 +0100

    Added basic layout for main app component. Added basic form to home component and included necessary module from angular library. Added author and description metadata tags to index.html head.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  newLinkForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.newLinkForm = new FormGroup({});
+  }
+
+  onSubmit() {
+
   }
 
 }
